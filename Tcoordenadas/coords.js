27b5c83f1c373bbca193fed79e23fd2917597d12
NicolasRiosM <69985870+NicolasRiosM@users.noreply.github.com>
2022-10-27 21:39:52 -0300

    push

--- Tcoordenadas/coords.js
+++ Tcoordenadas/coords.js
@@ -1,27 +1,34 @@
 
 const { Kafka } = require('kafkajs')
 
-const kafka = new Kafka({
-  clientId: 'my-app123',
-  brokers: ['kafka:9092'], //cambiar a kafka cuando sea docker
-})
+
 
 
 
 const coord = async () => {
-  const consumer = kafka.consumer({ groupId: 'test-group' })
+    const kafka = new Kafka({
+      clientId: 'my-app123',
+      brokers: ['kafka:9092'], //cambiar a kafka cuando sea docker
+    })
+    const consumer = kafka.consumer({ groupId: 'test-group221' })
 
-  await consumer.connect()
-  await consumer.subscribe({ topic: 'coordenadas', fromBeginning: true })
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'coordenadas', fromBeginning: true })
 
-  await consumer.run({
-    eachMessage: ({ topic, partition, message }) => {
-        console.log("Carrito: " + JSON.parse(message.value.toString()).carrito + "   Coordenadas: " + JSON.parse(message.value.toString()).coordenadas)
+    await consumer.run({
+        eachMessage: ({ topic, partition, message }) => {
+            console.log("Carrito: " + JSON.parse(message.value.toString()).carrito + "   Coordenadas: " + JSON.parse(message.value.toString()).coordenadas)
 
-    },
-    
-  })
+        },
+      
+    })
+    const EARLIEST_OFFSET = -2
 
+    const partitions = [0, 1, 2]
+
+    partitions.forEach((partition) => {
+        consumer.seek({ topic: 'coordenadas', offset: EARLIEST_OFFSET, partition })
+    })
 }
 
 module.exports = { coord }
