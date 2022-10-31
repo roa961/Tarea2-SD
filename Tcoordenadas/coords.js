6a4ea59a59380cce3d1cf2ce4a9caa9902ece8d5
NicolasRiosM <69985870+NicolasRiosM@users.noreply.github.com>
2022-10-30 23:09:18 -0300

    actual2

--- Tcoordenadas/coords.js
+++ Tcoordenadas/coords.js
@@ -1,38 +1,29 @@
 const { Kafka } = require('kafkajs')
-
 const kafka = new Kafka({
   clientId: 'my-app123',
   brokers: ['kafka:9092'], //cambiar a kafka cuando sea docker
 })
-
-
-
 const coord = async () => {
   const consumer = kafka.consumer({ groupId: "ubication" });
   console.log("Entra Ubication")
   await consumer.connect();
   await consumer.subscribe({ topic: "ubication", fromBeginning: true });
-  console.log("EL SIEGUIENTE ES EL MESSAGE")
   await consumer.run({
-
     eachMessage: async ({ topic, partition, message }) => {
-      console.log("CONDICIONES SIGUIENTES")
       if(partition == 0){
-        console.log("Este carrito no fue denunciado")
+        console.log("carrito no denunciado")
         console.log(value) 
       }
       else if(partition == 1)
       {
-        console.log("Este carrito fue denunciado, ES PROFUGO ATRAPENLOC CTMRE")
+        console.log("carrito denunciado")
         console.log(value)
       }
       else
       {
-        console.log("Estoy en la shit.")
+        console.log("error")
       }
     },
   })
-  console.log("Sali del Mesagge")
 }
-
 module.exports = { coord }
